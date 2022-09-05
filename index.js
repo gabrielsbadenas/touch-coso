f738536e93be2252d325b27cc5b138383763d05d
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-09-05 16:13:27 -0300

    add line to the server to serve public files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('hola mundo')
 const express = require('express'),
 	app = express(),
 	port = 3000
+app.use(express.static(process.cwd() + '/public'));
 app.get('/',(q,s)=>{
 s.send('helo world')
 })
