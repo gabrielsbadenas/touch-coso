02a86fb0028a8890910b984681eff294fb9d77f1
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2022-09-05 15:43:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('hola mundo')
+const express = require('express'),
+	app = express(),
+	port = 3000
+app.get('/',(q,s)=>{
+s.send('helo world')
+})
+app.listen(port,()=>{
+console.log('localhost/'+port)
+}
+)
